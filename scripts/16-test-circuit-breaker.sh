12b2014e42daa2e094d905cb67d7592d70b77891
Tebansus <mogtebanjim@gmail.com>
2026-06-14 18:58:02 -0600

    Esteban, Luis, Emanuel: Add high-concurrency traffic generation to circuit breaker test

--- scripts/16-test-circuit-breaker.sh
+++ scripts/16-test-circuit-breaker.sh
@@ -5,3 +5,6 @@ echo "Waiting for fortio pod to be ready..."
 kubectl wait pods -l app=fortio -n bookinfo --for=condition=Ready --timeout=90s
 
 FORTIO_POD=$(kubectl get pod -l app=fortio -n bookinfo -o jsonpath='{.items[0].metadata.name}')
+
+echo "Sending high concurrency traffic (20 connections, 100 total requests) to ratings via fortio..."
+kubectl exec "$FORTIO_POD" -c fortio -n bookinfo -- fortio load -c 20 -qps 0 -n 100 -loglevel Warning http://ratings:9080/ratings/0
